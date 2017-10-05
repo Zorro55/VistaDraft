6048eee075d83709ecf573a7ffeae5916962f4c9
Andrejka <Zorro55@gmail.com>
2017-10-05 08:16:25 +0300

    sec commit

--- WpfApp1/Properties/AssemblyInfo.cs
+++ WpfApp1/Properties/AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyInformationalVersion("13.3.1.74-g5224f3b")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
